465b104ccc1c62809ca2fbd9f0e344075a530645
Ximena1324 <castanedamximena@gmail.com>
2025-05-16 16:49:47 -0500

    Actualizo DatosUsuario.py con los últimos cambios

--- Backend/DatosUsuario.py
+++ Backend/DatosUsuario.py
@@ -11,23 +11,21 @@ def get_user_data(current_user: dict = Depends(get_current_user)):
     try:
         db = connect_to_mysql()
         query = "SELECT * FROM users WHERE email = %s"
-        result = execute_query(db, query, (current_user["email"],))  
+        result = execute_query(db, query, (current_user["email"],))
 
         if not result:
             raise HTTPException(status_code=404, detail="Usuario no encontrado")
 
         usuario_db = result[0]
 
-        usuario_respuesta = {
+        return {
             "id": usuario_db.get("id"),
             "username": usuario_db.get("Nusuario"),
             "email": usuario_db.get("email"),
-            "nombre": f'{usuario_db.get("nombre", "")} {usuario_db.get("apellido", "")}',
+            "nombre": f"{usuario_db.get('nombre', '')} {usuario_db.get('apellido', '')}",
             "telefono": usuario_db.get("telefono"),
-            "Ndocumento": usuario_db.get("Ndocumento")
+            "Ndocumento": usuario_db.get("Ndocumento"),
         }
-
-        return usuario_respuesta
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error interno del servidor: {str(e)}")
